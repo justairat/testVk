8bb7b63b1d35b3e8447d0fd8444e4784dbc147c4
Airat <just.airat@mail.ru>
2015-08-04 00:32:53 +0300

    add new feuture

--- app/src/main/java/com/example/airatim/testvk/MainActivity.java
+++ app/src/main/java/com/example/airatim/testvk/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.airatim.testvk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.vk.sdk.VKSdk;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VKSdk.initialize(getApplicationContext());
+            }
+        });
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
