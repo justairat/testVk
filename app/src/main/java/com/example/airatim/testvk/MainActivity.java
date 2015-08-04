7826986a5c3606b5d3ffbf3da02ba96bbd929438
Airat <just.airat@mail.ru>
2015-08-04 18:48:11 +0300

    inicialize SDK VK Api

--- app/src/main/java/com/example/airatim/testvk/MainActivity.java
+++ app/src/main/java/com/example/airatim/testvk/MainActivity.java
@@ -1,30 +1,85 @@
 package com.example.airatim.testvk;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.dialogs.VKShareDialog;
+import com.vk.sdk.util.VKUtil;
 
-public class MainActivity extends AppCompatActivity {
+
+
+
+public class MainActivity extends Activity{
+    private static final String[] sMyScope = new String[]{
+            VKScope.FRIENDS,
+            VKScope.WALL,
+            VKScope.PHOTOS,
+            VKScope.NOHTTPS
+    };
+
+
+
 Button btn;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         VKSdk.initialize(this);
-        
-        btn = (Button) findViewById(R.id.button);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        if(!VKSdk.isLoggedIn())
+            VKSdk.login(this, sMyScope);
+        else{
+            ShowDialog();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
             @Override
-            public void onClick(View v) {
+            public void onResult(VKAccessToken res) {
+                // Пользователь успешно авторизовался
+            }
+
+            @Override
+            public void onError(VKError error) {
+                // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void ShowDialog(){
 
 
+        VKShareDialog dlg = new VKShareDialog();
+        dlg.setText("test");
+        dlg.setAttachmentLink("Posted from app", "https://url.ru");
+        dlg.setShareDialogListener(new VKShareDialog.VKShareDialogListener() {
+            @Override
+            public void onVkShareComplete(int postId) {
+            }
+
+            @Override
+            public void onVkShareCancel() {
             }
         });
 
